320a2d5cfec09d56916f26681a4c0648c80acb8f
Suganth <suganthchandran07@gmail.com>
2024-10-23 15:10:54 +0530

    updated

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import '../styles/Banner.css'
+import banner from '../Assets/background.png'
 
 const Banner = () => {
   return (
     <div>
-      
+        <section className="home-image" style={{ backgroundImage: `url(${banner})` }}>
+                <div>
+                  
+                </div>
+        </section>
     </div>
   )
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import '../styles/Navbar.css'
+import { VscAccount } from "react-icons/vsc";
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className='navbar'>
+        <div className='navbar-start'>
+            <h1>MNTN</h1>
+        </div>
+
+        <div className='navbar-center'>
+                <div>
+                    <h1>Equipment</h1>
+                </div>
+
+                <div>
+                    <h1>About Us</h1>
+                </div>
+
+                <div>
+                    <h1>Blog</h1>
+                </div>
+        </div>
+
+        <div className='navbar-end'>
+            <VscAccount />
+            <h1>Account</h1>
+        </div>
     </div>
   )
 }
