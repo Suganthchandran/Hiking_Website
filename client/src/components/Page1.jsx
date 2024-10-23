f1cc9df8dc7ccb06584ba2427da9a555bf02dfe7
SWETHA-0945 <swethamk45@gmail.com>
2024-10-23 10:24:16 +0530

    updated

--- client/src/components/Page1.jsx
+++ client/src/components/Page1.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import img1 from '../Assets/01.png'
 import '../styles/Page1.css';
+import { FaArrowRightLong } from "react-icons/fa6";
+
 const Page1 = () => {
   return (
     <div className='page1'>
@@ -15,7 +17,12 @@ const Page1 = () => {
         <div className="page1-text-container">
         <h2>What level of Hike are you?</h2>
         <p>Determining what level of hiker you are can be an tool when planning future hikes. This hiking level guide will help you plan hikes according to different hike ratings set by various webistes like.</p>
-        <p>Read more</p></div>
+        
+        <div className="page1-arrow">
+           <div> <p>Read more</p> </div>
+            <div> <FaArrowRightLong /> </div>
+      </div>
+        </div>
       </div>
     <div className="page1-img">
         <img src={img1}/>
